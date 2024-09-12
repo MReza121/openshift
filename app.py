bb04d8b3286c5ffd20584448b24e22e7cd070883
unknown1 <rezayadollahi2019@gmail.com>
2024-09-12 19:38:12 +0200

    some small modification

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 # app.py
 from flask import Flask
+import pandas as pd
+
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, OpenShift with Docker!'
+
 @app.route('/')
 def hello_world():
-    return 'Hello, OpenShift with Docker!'
+    df = pd.DataFrame({'Data': [1, 2, 3]})
+    return df, 'Hello, OpenShift with Docker!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
