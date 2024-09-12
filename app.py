7e8badc12c7db7cd69f64fcbccb97ae83bffad95
unknown1 <rezayadollahi2019@gmail.com>
2024-09-12 20:59:14 +0200

    some small modification

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     df = pd.DataFrame({'Data': [1, 2, 3]})
-    return df, 'Hello, OpenShift with Docker!'
+    df.to_csv('MODEL_PATH1/test.csv')
+    return  'Hello, OpenShift with Docker!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
