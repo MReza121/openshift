5f4a9c876b4ca277626e61b02c37eaeab8eac20b
unknown1 <rezayadollahi2019@gmail.com>
2024-09-13 00:58:29 +0200

    some small modification

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def save_dataframe():
     df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
-    df.to_csv('/path/to/your/file.csv', index=False)
+    df.to_csv('/etc/mymodel', index=False)
 
 if __name__ == '__main__':
     save_dataframe()
